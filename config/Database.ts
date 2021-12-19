29918196a2c1be5aeec5dd7e5979b51e1d97fcd6
jenisraj76 <71476957+jenisraj76@users.noreply.github.com>
2021-12-19 21:40:56 +0530

    Update Database.ts

--- config/Database.ts
+++ config/Database.ts
@@ -2,7 +2,7 @@ export class Database {
     static readonly CONNECTION = process.env.TYPEORM_CONNECTION || "mysql";
     static readonly HOST = process.env.TYPEORM_HOST || "localhost";
     static readonly USERNAME = process.env.TYPEORM_USERNAME || "root";
-    static readonly PASSWORD = process.env.TYPEORM_PASSWORD || "jenis@raj76";
+    static readonly PASSWORD = process.env.TYPEORM_PASSWORD || "";
     static readonly DATABASE = process.env.TYPEORM_DATABASE || "";
     static readonly SCHEMA = process.env.TYPEORM_SCHEMA || "";
     static readonly PORT = Number(process.env.TYPEORM_PORT||'3306');
